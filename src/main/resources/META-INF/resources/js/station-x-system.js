e7424a028203d6612530e75955e4b75e3b149565
jerry <jerry@JerryDevelServer>
2025-09-08 14:51:26 +0900

    matched for v.0.7.5

--- src/main/resources/META-INF/resources/js/station-x-system.js
+++ src/main/resources/META-INF/resources/js/station-x-system.js
@@ -10,5 +10,22 @@ const SXSystem ={
 		newScript.type = 'module';
 		newScript.src = `${srcUrl}?v=${Date.now()}`; // cache-busting
 		document.head.appendChild(newScript);
-	}	
+	},
+	getUser: function(){
+		return {
+				userId: Liferay.ThemeDisplay.getUserId(),
+				userName: Liferay.ThemeDisplay.getUserName().replace(/\\u([\d\w]{4})/gi, (match, grp) => {
+					return String.fromCharCode(parseInt(grp, 16));
+				})
+		};
+	},
+	getLanguageId: function(){
+			return Liferay.ThemeDisplay.getLanguageId().replace(/_/g, "-");
+	},
+	getDefaultLanguageId: function(){
+			return Liferay.ThemeDisplay.getDefaultLanguageId().replace(/_/g, "-");
+	},
+	getAvailableLanguages: function(){
+		return Object.keys(Liferay.localization.available).map(lang=>lang.replace(/_/g, "-"));
+	}
 };
